faf2302527229f62a4d27d9b86b5db08f8a3000d
DawidCisinski <145262263+DawidCisinski@users.noreply.github.com>
2026-08-25 12:53:39 +0200

    Add files via upload

---
+++ pawsoft(v.2.3.1).lua
@@ -0,0 +1,506 @@
+local PARMS = { ... }
+local OPTIONS = {}
+local MODEM = peripheral.find("modem")
+local CLIENT_IP = os.getComputerID()
+
+
+local SERVERS_LIST = {}
+local SERVER_IP = -1
+
+local USER = { type = "none", password = "none", key = "none" }
+
+if not fs.exists("$options.txt") then
+    local f = fs.open("$options.txt", "w")
+    f.write("passwords @default_guest_password@")
+    f.close()
+end
+
+local f = fs.open("$options.txt", "r")
+local data = f.readAll()
+f.close()
+local passwords = {}
+for s in string.gmatch(data, "[^@]+") do
+    table.insert(passwords, s)
+end
+
+local GUEST_PASSWORD = passwords[2]
+
+--===========================================================
+
+if not MODEM then
+    print("FATAL ERROR: install modem")
+    error("", 0)
+end
+
+MODEM.open(CLIENT_IP)
+
+local function system_reset_options()
+    SERVERS_LIST = {}
+    SERVER_IP = -1
+    USER = { type = "none", password = "none", key = "none" }
+end
+
+--===========================================================
+
+local function clear()
+    term.clear()
+    term.setCursorPos(1, 1)
+    term.setTextColor(colors.white)
+end
+
+local function rprint(colorName, text, text2)
+    if not text2 then text2 = "" end
+    term.setTextColor(colors[colorName])
+    print(text, text2)
+    term.setTextColor(colors.white)
+end
+
+local function rwrite(colorName, text)
+    term.setTextColor(colors[colorName])
+    write(text)
+    term.setTextColor(colors.white)
+end
+
+
+local function dialog(title, options)
+    local selected = 1
+
+    while true do
+        clear()
+        rprint("yellow", "\n" .. title)
+        print("")
+
+        for i, opt in ipairs(options) do
+            if not (opt:sub(1, 1) == "$" or opt == "index.txt") then
+                if i == selected then
+                    rprint("yellow", ">> " .. opt)
+                else
+                    print("   " .. opt)
+                end
+            elseif opt:sub(1, 1) == "$" then
+                if i == selected then
+                    rprint("yellow", ">> " .. opt .. " (protected)")
+                else
+                    rprint("orange", "   " .. opt)
+                end
+            else
+                if i == selected then
+                    rprint("yellow", ">> " .. opt .. " (www)")
+                else
+                    rprint("blue", "   " .. opt)
+                end
+            end
+        end
+
+        local event, key = os.pullEvent("key")
+
+        if key == keys.up then
+            selected = selected - 1
+            if selected < 1 then selected = #options end
+        elseif key == keys.down then
+            selected = selected + 1
+            if selected > #options then selected = 1 end
+        elseif key == keys.enter then
+            return selected, options[selected]
+        end
+    end
+end
+
+local function rdialog(title, options)
+    local selected = 1
+
+    while true do
+        clear()
+        rprint("yellow", "\n" .. title)
+        print("")
+
+        for i, opt in ipairs(options) do
+            if i == selected then
+                rprint("yellow",
+                    ">> " ..
+                    "id: " ..
+                    opt.ip ..
+                    " / repo:" ..
+                    tostring(opt.repo) .. " / www:" .. tostring(opt.www) .. " / printer:" .. tostring(opt.printer))
+            else
+                print("   " ..
+                    "id: " ..
+                    opt.ip ..
+                    " / repo:" ..
+                    tostring(opt.repo) .. " / www:" .. tostring(opt.www) .. " / printer:" .. tostring(opt.printer))
+            end
+        end
+
+        local event, key = os.pullEvent("key")
+
+        if key == keys.up then
+            selected = selected - 1
+            if selected < 1 then selected = #options end
+        elseif key == keys.down then
+            selected = selected + 1
+            if selected > #options then selected = 1 end
+        elseif key == keys.enter then
+            return options[selected].ip
+        end
+    end
+end
+
+local function rinput(text, USER, pass)
+    local user_icon = "[N]"
+    if USER.type == "guest" then user_icon = "[G]" end
+    if USER.type == "user" then user_icon = "[U]" end
+    if USER.type == "admin" then user_icon = "[A]" end
+
+    print("")
+
+    rwrite("blue", user_icon .. " ")
+    rwrite("yellow", text .. " >> ")
+    return read(pass):gsub(" ", "")
+end
+
+local function reader(title, list_text)
+    clear()
+    rprint("yellow", "\n" .. title .. "\n")
+
+    local _, h = term.getSize()
+    local maxY = h - 2
+
+    for j, v in ipairs(list_text) do
+        print(v)
+
+        local _, y = term.getCursorPos()
+
+        if y >= maxY then
+            rprint("yellow", "\n[nextpage]")
+            os.pullEvent("key")
+            clear()
+            rprint("yellow", "\n" .. title .. "\n")
+        end
+    end
+end
+
+local function system_boot()
+    system_reset_options()
+    clear()
+    rprint("yellow", "Pawsoft v.2.3.1 (client)")
+    print("use [connect] to select server")
+    print("type [help] for commands")
+end
+
+--===========================================================
+
+local function server_info(message)
+    if message.info then
+        rprint("blue", "(server) Info:", message.info)
+    elseif message.ok == "_" then
+    elseif message.ok then
+        rprint("green", "(server)", message.ok)
+    else
+        rprint("red", "(server) Error:", message.error)
+    end
+end
+
+local function send(message_in, SERVER_IP)
+    message_in.password = USER.password
+    MODEM.transmit(tonumber(SERVER_IP), CLIENT_IP, message_in)
+
+    local timerID = os.startTimer(3)
+
+    while true do
+        local event, side, channel, replyChannel, message = os.pullEvent()
+
+        if event == "modem_message" then
+            if replyChannel == tonumber(SERVER_IP) then
+                server_info(message)
+                return message
+            end
+        end
+
+        if event == "timer" and side == timerID then
+            message = { error = "connection lost" }
+            server_info(message)
+            sleep(2)
+            system_boot()
+            return message
+        end
+    end
+end
+
+
+local function multi_send()
+    local found = { { ip = -1, repo = "null", www = "null", printer = "null" } }
+
+    for ip = 0, 100 do
+        MODEM.transmit(ip, CLIENT_IP, { cmd = "ping" })
+    end
+
+    local timerID = os.startTimer(3)
+
+    while true do
+        local event, side, channel, replyChannel, message = os.pullEvent()
+
+        if event == "modem_message" then
+            if type(message) == "table" and message.ok == "pong" then
+                table.insert(found, {
+                    ip = replyChannel,
+                    repo = message.repo,
+                    www = message.www,
+                    printer = message.printer
+                })
+            end
+        end
+
+        if event == "timer" and side == timerID then
+            return found
+        end
+    end
+end
+
+--===========================================================
+
+local function hash64(str)
+    local h1 = 0x811C9DC5
+    local h2 = 0x243F6A88
+    local p1 = 0x01000193
+    local p2 = 0x85EBCA6B
+
+    for i = 1, #str do
+        local b = string.byte(str, i)
+
+        h1 = bit32.bxor(h1, b)
+        h1 = (h1 * p1) % 0x100000000
+        local r1 = bit32.lshift(h1, 5) % 0x100000000
+        local l1 = bit32.rshift(h1, 2)
+        h1 = bit32.bxor(r1, l1)
+
+        h2 = bit32.bxor(h2, b)
+        h2 = (h2 * p2) % 0x100000000
+        local r2 = bit32.lshift(h2, 7) % 0x100000000
+        local l2 = bit32.rshift(h2, 3)
+        h2 = bit32.bxor(r2, l2)
+    end
+
+    return string.format("%08x%08x", h1, h2)
+end
+
+local function keyBytes(key)
+    local t = {}
+    for i = 1, #key do
+        t[i] = string.byte(key, i)
+    end
+    return t
+end
+
+
+local function xorStream(data, key)
+    local kb = keyBytes(key)
+    local klen = #kb
+    local out = {}
+
+    for i = 1, #data do
+        local b = string.byte(data, i)
+        local k = kb[((i - 1) % klen) + 1]
+        out[i] = string.char(bit32.bxor(b, k))
+    end
+
+    return table.concat(out)
+end
+
+
+function encrypt(str, key)
+    return xorStream(str, key)
+end
+
+function decrypt(str, key)
+    return xorStream(str, key)
+end
+
+--===========================================================
+
+local function connect(dcon)
+    local selected
+    local password
+
+    if not dcon then
+        clear()
+        write("Please wait...")
+        selected = rdialog("Select server", multi_send())
+    else
+        clear()
+        selected = rinput("Enter server IP", USER)
+    end
+
+    if selected == -1 then selected = 101 end
+
+    local _, name = dialog("Select account type", { "guest", "user", "admin" })
+    if name == "guest" then
+        password = GUEST_PASSWORD
+    else
+        password = rinput("Enter password", USER, "*")
+    end
+
+    local msg = {
+        cmd = "_registration",
+        user_type = name
+    }
+    local msg_out = send(msg, selected)
+    if msg_out.scode then
+        USER.password = hash64(password .. msg_out.scode)
+        USER.key = hash64(password)
+        local msg_out = send({ cmd = "_login", }, selected)
+        if msg_out.ok then
+            USER.type = name
+            SERVER_IP = selected
+        end
+    end
+end
+
+local function list(only_list)
+    local msg = send({ cmd = "list" }, SERVER_IP)
+
+    if type(msg.list) == "table" then
+        if not only_list then
+            print("Files on disk:")
+            for _, v in ipairs(msg.list) do
+                if v:sub(1, 1) == "$" then
+                    write(" - ")
+                    rprint("orange", v)
+                elseif v == "index.txt" then
+                    write(" - ")
+                    rprint("blue", v)
+                else
+                    print(" - " .. v)
+                end
+            end
+        else
+            return msg.list
+        end
+    end
+end
+
+local function get(type)
+    local file
+
+    if type == 2 then
+        file = "index.txt"
+    else
+        _, file = dialog("Select file", list(true))
+    end
+
+    local msg_out = send({
+        cmd = "get",
+        file = file,
+    }, SERVER_IP)
+
+    if msg_out.ok then
+        local data = decrypt(msg_out.data, USER.key)
+        if not type then
+            local f = fs.open(file, "w")
+            rprint("green", "Downloaded: " .. file)
+            f.write(data)
+            f.close()
+        elseif type == 1 then
+            local f = fs.open("temp.txt", "w")
+            f.write(data)
+            f.close()
+
+            clear()
+
+            shell.run("edit", "temp.txt")
+        elseif type == 2 then
+            local lines = {}
+            for line in data:gmatch("[^\r\n]+") do
+                lines[#lines + 1] = line
+            end
+            reader("Webbrowser", lines)
+        end
+    end
+end
+
+local function push(print)
+    local files = fs.list("/")
+    local i = 1
+    while i <= #files do
+        local f = files[i]
+        if not f:lower():match("%.txt$") then
+            table.remove(files, i)
+        else
+            i = i + 1
+        end
+    end
+
+    local _, file = dialog("Select file", files)
+
+    if not fs.exists(file) then
+        print("File does not exist:", file)
+        return
+    end
+
+    local f = fs.open(file, "r")
+    local data = encrypt(f.readAll(), USER.key)
+    f.close()
+
+    local cmd = "push"
+    if print then cmd = "print" end
+
+    local res = send({
+        cmd = cmd,
+        file = file,
+        data = data,
+    }, SERVER_IP)
+end
+
+local help = {
+    "help    - Show available commands",
+    "connect - Connect to remote server",
+    "con     - Alias for connect",
+    "dcon    - Direct connect to remote server",
+    "list    - List available files",
+    "get     - Download file",
+    "open    - Download and open file",
+    "www     - Open web resource",
+    "push    - Upload file",
+    "print   - Send file to printer",
+    "stop    - Stop server",
+    "exit    - Exit application"
+}
+
+--===========================================================
+
+system_boot()
+
+
+while true do
+    local cmd = rinput("server " .. SERVER_IP, USER)
+
+    if cmd == "exit" then
+        break
+    elseif cmd == "connect" or cmd == "con" then
+        connect()
+    elseif cmd == "dconnect" or cmd == "dcon" then
+        connect(true)
+    else
+        if SERVER_IP == -1 then
+            server_info({ error = "connection lost" })
+        elseif cmd == "list" then
+            list()
+        elseif cmd == "get" then
+            get()
+        elseif cmd == "open" then
+            get(1)
+        elseif cmd == "www" then
+            get(2)
+        elseif cmd == "push" then
+            push()
+        elseif cmd == "print" then
+            push(true)
+        elseif cmd == "help" then
+            reader("Help", help)
+        else
+            send({ cmd = cmd }, SERVER_IP)
+        end
+    end
+end
+
+rprint("blue", "client closed\n")
+MODEM.close(CLIENT_IP)
---
+++ pawsort-server(v.2.3.1).lua
@@ -0,0 +1,464 @@
+local PARMS = { ... }
+local MODEM = peripheral.find("modem")
+local SERVER_IP = os.getComputerID()
+
+--===========================================================
+
+local USER = 0
+local PASSWORDS = {}
+PASSWORDS["guest"] = { power = 1, password = PARMS[3] }
+PASSWORDS["user"] = { power = 2, password = PARMS[2] }
+PASSWORDS["admin"] = { power = 3, password = PARMS[1] }
+
+local SESSIONS = {}
+
+if not fs.exists("$options.txt") then
+    local f = fs.open("$options.txt", "w")
+    f.write("passwords @default_guest_password@user_password@admin_password@")
+    f.close()
+end
+
+
+local f = fs.open("$options.txt", "r")
+local data = f.readAll()
+f.close()
+local passwords = {}
+for s in string.gmatch(data, "[^@]+") do
+    table.insert(passwords, s)
+end
+
+
+if not PARMS[1] then PARMS[1] = passwords[4] end
+if not PARMS[2] then PARMS[2] = passwords[3] end
+if not PARMS[3] then PARMS[3] = passwords[2] end
+
+PASSWORDS["guest"].password = PARMS[3]
+PASSWORDS["user"].password = PARMS[2]
+PASSWORDS["admin"].password = PARMS[1]
+
+if not MODEM then
+    print("FATAL ERROR: install modem")
+    error("", 0)
+end
+
+MODEM.open(SERVER_IP)
+
+--===========================================================
+
+local function getDiskPath()
+    if fs.exists("disk") and fs.isDir("disk") then
+        return "disk", true
+    end
+    return "none"
+end
+
+local function hasWebsite()
+    if fs.exists("disk/index.txt") then return true end
+    return false
+end
+
+local _, hasRepo = getDiskPath()
+
+local function hasPrinter()
+    if peripheral.find("printer") then
+        return true
+    else
+        return false
+    end
+end
+
+--===========================================================
+
+local function clear()
+    term.clear()
+    term.setCursorPos(1, 1)
+    term.setTextColor(colors.white)
+end
+
+local function rprint(colorName, text, text2)
+    if not text2 then text2 = "" end
+    term.setTextColor(colors[colorName])
+    print(text, text2)
+    term.setTextColor(colors.white)
+end
+
+local function line()
+    local w = term.getSize()
+    print(string.rep("-", w))
+end
+
+local function serverprint(text)
+    print("(server): " .. text)
+end
+
+--===========================================================
+
+local function system_info(full_info)
+    clear()
+    rprint("yellow", "Pawsoft v.2.3.1 (server)")
+    print("pawsoft-server [admin password] [user password]")
+    print("type [help] for commands\n")
+    if full_info then
+        print(" Server IP: " .. SERVER_IP)
+        print(" Admin password: " .. PASSWORDS["admin"].password)
+        print(" User password: " .. PASSWORDS["user"].password)
+        print(" Guest password: " .. PASSWORDS["guest"].password)
+        print(" Website: " .. tostring(hasWebsite()))
+        print(" Printer: " .. tostring(hasPrinter()))
+        print(" Repo disk patch: /" .. getDiskPath() .. "\n")
+
+        line()
+        print(
+            "index.txt on the disk enables the www server.\nFiles starting with '$' are protected\nand require a valid login.")
+        line()
+    end
+end
+
+local function hash64(str)
+    local h1 = 0x811C9DC5
+    local h2 = 0x243F6A88
+    local p1 = 0x01000193
+    local p2 = 0x85EBCA6B
+
+    for i = 1, #str do
+        local b = string.byte(str, i)
+
+        h1 = bit32.bxor(h1, b)
+        h1 = (h1 * p1) % 0x100000000
+        local r1 = bit32.lshift(h1, 5) % 0x100000000
+        local l1 = bit32.rshift(h1, 2)
+        h1 = bit32.bxor(r1, l1)
+
+        h2 = bit32.bxor(h2, b)
+        h2 = (h2 * p2) % 0x100000000
+        local r2 = bit32.lshift(h2, 7) % 0x100000000
+        local l2 = bit32.rshift(h2, 3)
+        h2 = bit32.bxor(r2, l2)
+    end
+
+    return string.format("%08x%08x", h1, h2)
+end
+
+local function keyBytes(key)
+    local t = {}
+    for i = 1, #key do
+        t[i] = string.byte(key, i)
+    end
+    return t
+end
+
+
+local function xorStream(data, key)
+    local kb = keyBytes(key)
+    local klen = #kb
+    local out = {}
+
+    for i = 1, #data do
+        local b = string.byte(data, i)
+        local k = kb[((i - 1) % klen) + 1]
+        out[i] = string.char(bit32.bxor(b, k))
+    end
+
+    return table.concat(out)
+end
+
+
+function encrypt(str, key)
+    return xorStream(str, key)
+end
+
+function decrypt(str, key)
+    return xorStream(str, key)
+end
+
+local function addRecord(client_ip, user_type, scode)
+    SESSIONS[client_ip] = {
+        password = hash64(PASSWORDS[user_type].password .. scode),
+        ip = client_ip,
+        power = PASSWORDS[user_type].power,
+        key = hash64(PASSWORDS[user_type].password)
+    }
+end
+
+local function verification(SESSIONS, message, senderIP)
+    local session = SESSIONS[senderIP]
+
+    if not session then
+        return false
+    end
+
+    if session.ip ~= senderIP then
+        return false
+    end
+
+    if type(message.password) ~= "string" then
+        return false
+    end
+
+    if message.password == session.password then
+        return true
+    end
+
+    return false
+end
+
+
+--===========================================================
+
+local function ping(senderIP, msg)
+    MODEM.transmit(senderIP, SERVER_IP, { ok = "pong", repo = hasRepo, www = hasWebsite(), printer = hasPrinter() })
+end
+
+
+local function _registration(senderIP, msg)
+    if msg.user_type ~= "guest" and
+        msg.user_type ~= "user" and
+        msg.user_type ~= "admin" then
+        MODEM.transmit(senderIP, SERVER_IP, { error = "invalid user type" })
+        return
+    end
+
+    local SCODE = math.random(1000, 9999)
+    addRecord(senderIP, msg.user_type, SCODE)
+    MODEM.transmit(senderIP, SERVER_IP, { scode = SCODE, ok = "_" })
+    serverprint("ip:" .. SESSIONS[senderIP].ip .. " password: " .. SESSIONS[senderIP].password) -- TEST
+end
+
+
+local function _login(senderIP, msg)
+    if verification(SESSIONS, msg, senderIP) then
+        MODEM.transmit(senderIP, SERVER_IP, { ok = "Logged in" })
+    else
+        MODEM.transmit(senderIP, SERVER_IP, { error = "Access denied" })
+    end
+end
+
+local function list(senderIP, msg)
+    if getDiskPath() == "none" then
+        MODEM.transmit(senderIP, SERVER_IP, {
+            error = "No disk inserted"
+        })
+    else
+        MODEM.transmit(senderIP, SERVER_IP, { list = fs.list(getDiskPath()), ok = "_" })
+    end
+end
+
+local function get(senderIP, msg, USER)
+    if not (msg.file:sub(1, 1) == "$" and USER < 2) then
+        if getDiskPath() == "none" then
+            MODEM.transmit(senderIP, SERVER_IP, {
+                error = "No disk inserted"
+            })
+        else
+            local path = getDiskPath() .. "/" .. msg.file:gsub("/", "")
+            if fs.exists(path) then
+                local f = fs.open(path, "r")
+                local data = f.readAll()
+                f.close()
+
+                MODEM.transmit(
+                    senderIP,
+                    SERVER_IP,
+                    { data = encrypt(data, SESSIONS[senderIP].key), ok = "_" }
+                )
+            else
+                MODEM.transmit(
+                    senderIP,
+                    SERVER_IP,
+                    { error = "bad path" }
+                )
+            end
+        end
+    else
+        MODEM.transmit(
+            senderIP,
+            SERVER_IP,
+            { error = "Access denied" }
+        )
+    end
+end
+
+local function push(senderIP, msg, USER)
+    if not ((msg.file == "index.txt" or msg.file:sub(1, 1) == "$") and USER < 3) then
+        if not getDiskPath() then
+            MODEM.transmit(senderIP, SERVER_IP, {
+                error = "No disk inserted"
+            })
+        elseif fs.isReadOnly(getDiskPath()) then
+            MODEM.transmit(senderIP, SERVER_IP, {
+                error = "Disk is read-only"
+            })
+        else
+            local path = getDiskPath() .. "/" .. msg.file:gsub("/", "")
+
+            local f = fs.open(path, "w")
+            f.write(decrypt(msg.data, SESSIONS[senderIP].key))
+            f.close()
+
+            MODEM.transmit(senderIP, SERVER_IP, {
+                ok = "saved"
+            })
+        end
+    else
+        MODEM.transmit(
+            senderIP,
+            SERVER_IP,
+            { error = "Access denied" }
+        )
+    end
+end
+
+local function remoteprint(senderIP, msg, USER)
+    local printer = peripheral.find("printer")
+
+    if not printer then
+        MODEM.transmit(senderIP, SERVER_IP, {
+            error = "No printer connected"
+        })
+    elseif not printer.newPage() then
+        MODEM.transmit(senderIP, SERVER_IP, {
+            error = "No paper"
+        })
+    else
+        for line in msg.data:gmatch("[^\r\n]+") do
+            printer.write(line)
+            local _, y = printer.getCursorPos()
+            printer.setCursorPos(1, y + 1)
+        end
+
+        printer.endPage()
+
+        MODEM.transmit(senderIP, SERVER_IP, {
+            ok = "printed"
+        })
+    end
+end
+
+local function stop(senderIP, msg, USER)
+    MODEM.transmit(senderIP, SERVER_IP, {
+        ok = "Server stopped"
+    })
+    error("", 0)
+end
+
+
+local COMMANDS = {
+    { name = "_registration", starter = _registration, class = -1 },
+    { name = "_login",        starter = _login,        class = -1 },
+    { name = "ping",          starter = ping,          class = -1 },
+    { name = "list",          starter = list,          class = 1 },
+    { name = "get",           starter = get,           class = 1 },
+    { name = "print",         starter = remoteprint,   class = 2 },
+    { name = "push",          starter = push,          class = 2 },
+    { name = "stop",          starter = stop,          class = 3 }
+
+}
+
+--===========================================================
+
+local function server_loop()
+    while true do
+        local _, _, receiverIP, senderIP, msg = os.pullEvent("modem_message")
+        if receiverIP == SERVER_IP and type(msg) == "table" then
+            USER = 0
+
+            if verification(SESSIONS, msg, senderIP) then
+                USER = SESSIONS[senderIP].power
+            end
+
+            serverprint("p: " .. USER)
+
+            local found = false
+
+            for _, command in ipairs(COMMANDS) do
+                if command.name == msg.cmd then
+                    found = true
+                    if USER >= command.class then
+                        command.starter(senderIP, msg, USER)
+                    else
+                        MODEM.transmit(senderIP, SERVER_IP, { error = "invalid user" })
+                    end
+                    break
+                end
+            end
+
+            if not found then
+                MODEM.transmit(senderIP, SERVER_IP, { error = "unknown command" })
+            end
+        end
+    end
+end
+
+--===========================================================
+
+local function mount(emu)
+    local f = fs.open("startup.txt", "w")
+
+    if emu then
+        f.write([[
+ccemux.attach("right", "disk_drive", { id = 0 })
+
+ccemux.attach("left", "disk_drive", { id = 1 })
+
+ccemux.attach("top", "wireless_modem", {
+    range = 64,
+    interdimensional = false,
+    world = "main",
+    posX = 0,
+    posY = 0,
+    posZ = 0,
+})
+
+shell.run("pawsort-server.lua")
+]])
+    else
+        f.write([[
+shell.run("pawsort-server.lua")
+]])
+    end
+
+    f.close()
+
+    if fs.exists("startup.lua") then
+        fs.delete("startup.lua")
+    end
+
+    fs.move("startup.txt", "startup.lua")
+end
+
+local function help()
+    clear()
+    print("Commands:")
+    print("")
+    print("  help       Display this help message")
+    print("  info       Show system information")
+    print("  mount      Mount server in startup.lua")
+    print("  emumount   Mount server in startup.lua for CCEmuX")
+    print("  clear      Clear the terminal screen")
+    print("  cls        Alias for clear")
+    print("  stop       Exit the program")
+end
+
+--===========================================================
+
+system_info(true)
+
+local function console_loop()
+    while true do
+        --local cmd = rinput("server")
+        local cmd = read()
+        if cmd == "stop" then return end
+        if cmd == "info" then system_info(true) end
+        if cmd == "help" then help() end
+        if cmd == "emumount" then mount(true) end
+        if cmd == "mount" then mount(false) end
+        if cmd == "clear" or cmd == "cls" then clear() end
+    end
+end
+
+--===========================================================
+
+parallel.waitForAny(server_loop, console_loop)
+
+rprint("blue", "server closed\n")
+MODEM.close(SERVER_IP)
